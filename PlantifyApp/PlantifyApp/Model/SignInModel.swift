02b95a9b7c36d5c28f6cd1d6a5badeda439019d2
lsh1202 <vic071202@gmail.com>
2024-07-16 20:29:48 +0900

    :sparkles: :: 로그인 뷰 개발

---
+++ PlantifyApp/PlantifyApp/Model/SignInModel.swift
@@ -0,0 +1,13 @@
+//
+//  SignInModel.swift
+//  PlantifyApp
+//
+//  Created by hyk on 7/16/24.
+//
+
+import Foundation
+
+struct SignInModel: Codable {
+    var id: String = ""
+    var pw: String = ""
+}
---
+++ PlantifyApp/PlantifyApp/ViewModel/SignInViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SignInViewModel.swift
+//  PlantifyApp
+//
+//  Created by hyk on 7/16/24.
+//
+
+import Foundation
+
+class SignInViewModel: ObservableObject {
+    @Published var model = SignInModel()
+}
